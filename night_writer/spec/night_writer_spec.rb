8611ec168ab017573a8b415b2117d8de80d6bef5
Meredith Trevino <meredithtrevino@gmail.com>
2023-01-14 13:33:50 -0700

    Adds translate_to_braille

--- night_writer/spec/night_writer_spec.rb
+++ night_writer/spec/night_writer_spec.rb
@@ -15,4 +15,8 @@ RSpec.describe NightWriter do
     expect(night_writer.read_file).to eq('./message.txt')
     expect(night_writer.write_file).to eq('./braille.txt')
   end
+
+  it 'can take in a letter and translate to braille' do
+    expect(night_writer.translate_to_braille("a")).to eq(["0.", "..", ".."])
+  end
 end
